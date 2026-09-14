acc617192d9078691be439edfb4c912d070cee81
Aluno Alagoinhas <aluno.alagoinhas@senaibahia.com.br>
2026-09-14 14:32:13 -0300

    cria projeto com testes pytest

---
+++ app/calculadora.py
@@ -0,0 +1,10 @@
+def somar (a,b):
+    return a + b
+
+
+def subtrair(a,b):
+    return a - b
+
+def calcular_descont(preco,percentual):
+    desconto = preco * percentual / 100
+    return preco - desconto
